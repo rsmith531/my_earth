acd09b0bcd7f17616845103926b3763dc1eed31f
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-05-01 00:56:56 -0400

    add button component

---
+++ components/ui/button.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { userEvent, within, expect } from '@storybook/test';
+import { Button } from './button';
+import { Send } from 'lucide-react';
+const meta: Meta<typeof Button> = {
+  component: Button,
+};
+
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    children: 'test label',
+  },
+};
+
+export const Icon: Story = {
+    args: {
+      children: <Send />,
+    },
+};
+
+export default meta;
