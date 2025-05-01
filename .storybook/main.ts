dde1587fc1119c19869fca88ce40a0a93cd24f47
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-05-01 19:24:05 -0400

    storybook backgrounds addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/experimental-addon-test',
+    '@storybook/addon-backgrounds',
   ],
   framework: {
     name: '@storybook/react-vite',
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,6 +4,14 @@ import '../src/styles.css';
 const preview: Preview = {
   tags: ['autodocs'],
   parameters: {
+    backgrounds: {
+      values: [
+        { name: 'Space', value: '#000' },
+        { name: 'Night', value: '#0f172a' },
+        { name: 'Moonless Night', value: '#020617' },
+      ],
+      default: 'Moonless Night',
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
