2c2a6a57497a3c7ede0a7dc7d81ab59777534e72
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-05-01 23:38:46 -0400

    globe autorotate and background image

--- components/section/Globe.tsx
+++ components/section/Globe.tsx
@@ -1,16 +1,39 @@
-import UnderGlobe from 'react-globe.gl';
+import { useEffect, useRef, useState } from 'react';
+import UnderGlobe, { type GlobeMethods } from 'react-globe.gl';
 
 // https://thenewstack.io/recreating-shopifys-bfcm-globe-using-react-globe-gl/
 
 function Globe({ position }: { position: number }) {
+  const globeEl = useRef<GlobeMethods | undefined>(undefined);
+  const [autoSpin, setAutoSpin] = useState<boolean>(true);
+
+  /**
+   * when the component mounts, set it to autorotate the globe
+   */
+  useEffect(() => {
+    if (!globeEl?.current) return;
+    globeEl.current.controls().autoRotate = autoSpin;
+    globeEl.current.controls().autoRotateSpeed = 0.1;
+  }, [autoSpin]);
+
+  /**
+   * makes the globe start spinning again when the view is reset
+   */
+  useEffect(() => {
+    if (position === 100) {
+      setAutoSpin(true);
+      globeEl?.current?.pointOfView({ altitude: 2 }, 1000);
+    }
+  }, [position]);
+
   return (
-    <div
-      data-testId={'globe-root-element'}
-      //   className="h-screen w-screen flex place-items-center justify-center flex-col gap-4"
-      className="cursor-move"
-    >
+    <div data-testId={'globe-root-element'} className="cursor-move">
       <UnderGlobe
+        // disable autorotate when the user clicks the globe
+        onGlobeClick={() => setAutoSpin(false)}
+        ref={globeEl}
         globeImageUrl="//cdn.jsdelivr.net/npm/three-globe/example/img/earth-night.jpg"
+        backgroundImageUrl="//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png"
         globeOffset={[0, 500]}
         backgroundColor="#00000000"
       />
