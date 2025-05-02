17c1cc5bd2a8122ba0856be5b8af23dc0ca0ae39
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-05-01 20:55:07 -0400

    create AddReasonForm component

---
+++ components/section/AddReasonForm.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { AddReasonForm } from './AddReasonForm';
+
+const meta: Meta<typeof AddReasonForm> = {
+  component: AddReasonForm,
+};
+
+type Story = StoryObj<typeof AddReasonForm>;
+
+export const Default: Story = {
+  args: {
+    submitCallback: async (values) => {
+      console.log('[stories/AddReasonForm] got form values: ', values);
+    },
+  },
+};
+
+export default meta;
---
+++ components/section/AddReasonForm.tsx
@@ -0,0 +1,67 @@
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Button } from '@components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@components/ui/form';
+import { Send } from 'lucide-react';
+import { Textarea } from '@components/ui/textarea';
+
+const validationSchema = z.object({
+  message: z.string().min(1).max(300).trim(),
+});
+
+function AddReasonForm({
+  submitCallback,
+}: {
+  submitCallback: (values: z.infer<typeof validationSchema>) => Promise<void>;
+}) {
+  const form = useForm<z.infer<typeof validationSchema>>({
+    mode: 'onChange',
+    resolver: zodResolver(validationSchema),
+    defaultValues: {
+      message: '',
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(submitCallback)}
+        style={{ display: 'flex', gap: 16 }}
+      >
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem style={{ flexGrow: 1 }}>
+              <FormControl>
+                <Textarea
+                  placeholder="Tell us..."
+                  rows={2}
+                  {...field}
+                  style={{ resize: 'none' }}
+                  className="border-3 border-slate-700 hover:border-slate-700/90 bg-slate-200"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className={'aspect-square h-16 bg-slate-700 hover:bg-slate-700/90'}
+        >
+          <Send className="size-8 stroke-slate-200" />
+        </Button>
+      </form>
+    </Form>
+  );
+}
+
+export { AddReasonForm };
