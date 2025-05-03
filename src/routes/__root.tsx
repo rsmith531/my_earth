833622c10fff4b24d2ce3287754072cb1b74014e
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-05-03 11:02:23 -0400

    add sonner toast

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,13 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { Toaster } from '@components/ui/sonner';
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <Toaster richColors />
       <Outlet />
       <TanStackRouterDevtools />
     </>
   ),
-})
+});
