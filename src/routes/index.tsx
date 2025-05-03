bed74ca89e23d465e6430b19f0a50036c5dd7e20
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-05-02 20:13:05 -0400

    remove unused files and swap in actual home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,36 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
-import '../App.css'
+import { createFileRoute } from '@tanstack/react-router';
+import { Home } from '@components/page/Home';
 
 export const Route = createFileRoute('/')({
-  component: App,
-})
+  component: HomePage,
+});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="App-link"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
-    </div>
-  )
+function HomePage() {
+  const submissionCallback: Parameters<typeof Home>[0]['submitCallback'] =
+    async (values) => {
+      console.log('[index] got values', values);
+    };
+
+  return <Home submitCallback={submissionCallback} />;
 }
