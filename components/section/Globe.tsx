e05d22c348553c938120d41fb23bec9526c62b95
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-05-03 12:24:59 -0400

    start point for moving globe camera on interactive

--- components/section/Globe.tsx
+++ components/section/Globe.tsx
@@ -6,6 +6,7 @@ import UnderGlobe, { type GlobeMethods } from 'react-globe.gl';
 function Globe({ interactive }: { interactive: boolean }) {
   const globeEl = useRef<GlobeMethods | undefined>(undefined);
   const [autoSpin, setAutoSpin] = useState<boolean>(true);
+  const [verticalOffset, setVerticalOffset] = useState<number>(500);
 
   /**
    * when the component mounts, set it to autorotate the globe
@@ -16,13 +17,17 @@ function Globe({ interactive }: { interactive: boolean }) {
     globeEl.current.controls().autoRotateSpeed = 0.1;
   }, [autoSpin]);
 
+  // TODO: move the globe into the middle of the screen on interactive smoothly
   /**
    * makes the globe start spinning again when the view is reset
    */
   useEffect(() => {
     if (!interactive) {
       setAutoSpin(true);
+      // setVerticalOffset(500);
       globeEl?.current?.pointOfView({ altitude: 2 }, 1000);
+    } else {
+      // setVerticalOffset(0);
     }
   }, [interactive]);
 
@@ -34,7 +39,7 @@ function Globe({ interactive }: { interactive: boolean }) {
         ref={globeEl}
         globeImageUrl="//cdn.jsdelivr.net/npm/three-globe/example/img/earth-night.jpg"
         backgroundImageUrl="//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png"
-        globeOffset={[0, 500]}
+        globeOffset={[0, verticalOffset]}
         backgroundColor="#00000000" // transparent
       />
     </div>
