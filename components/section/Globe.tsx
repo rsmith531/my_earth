c5953921d8de03d0e517439fa7d1de8fab394d26
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-05-04 18:15:45 -0400

    add day/night cycle to globe

--- components/section/Globe.tsx
+++ components/section/Globe.tsx
@@ -1,12 +1,17 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import UnderGlobe, { type GlobeMethods } from 'react-globe.gl';
+import { TextureLoader, ShaderMaterial, Vector2 } from 'three';
+// @ts-expect-error no type :(
+import * as solar from 'solar-calculator';
 
 // https://thenewstack.io/recreating-shopifys-bfcm-globe-using-react-globe-gl/
+// day/night cycle code: https://github.com/vasturiano/react-globe.gl/blob/master/example/day-night-cycle/index.html
 
 function Globe({ interactive }: { interactive: boolean }) {
   const globeEl = useRef<GlobeMethods | undefined>(undefined);
   const [autoSpin, setAutoSpin] = useState<boolean>(true);
   const [verticalOffset, setVerticalOffset] = useState<number>(0);
+  const [globeMaterial, setGlobeMaterial] = useState<ShaderMaterial>();
 
   /**
    * when the component mounts, set it to autorotate the globe
@@ -17,7 +22,6 @@ function Globe({ interactive }: { interactive: boolean }) {
     globeEl.current.controls().autoRotateSpeed = 0.1;
   }, [autoSpin]);
 
-  // TODO: move the globe into the middle of the screen on interactive smoothly
   /**
    * makes the globe start spinning again when the view is reset
    */
@@ -30,7 +34,6 @@ function Globe({ interactive }: { interactive: boolean }) {
   }, [interactive]);
 
   const animationFrameId = useRef<number | null>(null);
-
   /**
    * gradually brings verticalOffset to 0 or 500 using a linear or cubic
    * ease-in-out animation.
@@ -89,19 +92,138 @@ function Globe({ interactive }: { interactive: boolean }) {
     animationFrameId.current = requestAnimationFrame(animateScroll);
   };
 
+  /**
+   * when the component mounts, gets the textures that are going to wrap the
+   * globe object and sets them as the globe material
+   */
+  useEffect(() => {
+    Promise.all([
+      new TextureLoader().loadAsync(
+        '//cdn.jsdelivr.net/npm/three-globe/example/img/earth-day.jpg',
+      ),
+      new TextureLoader().loadAsync(
+        '//cdn.jsdelivr.net/npm/three-globe/example/img/earth-night.jpg',
+      ),
+    ]).then(([dayTexture, nightTexture]) => {
+      setGlobeMaterial(
+        new ShaderMaterial({
+          uniforms: {
+            dayTexture: { value: dayTexture },
+            nightTexture: { value: nightTexture },
+            sunPosition: { value: new Vector2() },
+            globeRotation: { value: new Vector2() },
+          },
+          vertexShader: dayNightShader.vertexShader,
+          fragmentShader: dayNightShader.fragmentShader,
+        }),
+      );
+    });
+  }, []);
+
+  /**
+   * Updates the globe material's sun position uniform on an interval.
+   * Runs only when globeMaterial is available.
+   */
+  useEffect(() => {
+    if (!globeMaterial) return;
+
+    // initial update
+    globeMaterial.uniforms.sunPosition.value.set(...sunPosAt(+new Date()));
+
+    // update every 5 seconds after that
+    const intervalId = setInterval(() => {
+      globeMaterial.uniforms.sunPosition.value.set(...sunPosAt(+new Date()));
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, [globeMaterial]);
+
   return (
     <div data-testid={'globe-root-element'} className="cursor-move">
       <UnderGlobe
         // disable autorotate when the user clicks the globe
         onGlobeClick={() => setAutoSpin(false)}
+        globeMaterial={globeMaterial}
         ref={globeEl}
-        globeImageUrl="//cdn.jsdelivr.net/npm/three-globe/example/img/earth-night.jpg"
         backgroundImageUrl="//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png"
         globeOffset={[0, verticalOffset]}
         backgroundColor="#00000000" // transparent
+        onZoom={useCallback(
+          ({ lng, lat }: { lng: number; lat: number }) =>
+            globeMaterial?.uniforms.globeRotation.value.set(lng, lat),
+          [globeMaterial],
+        )}
       />
     </div>
   );
 }
+
+// Custom shader:  Blends night and day images to simulate day/night cycle
+const dayNightShader = {
+  vertexShader: `
+    varying vec3 vNormal;
+    varying vec2 vUv;
+    void main() {
+      vNormal = normalize(normalMatrix * normal);
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  fragmentShader: `
+    #define PI 3.141592653589793
+    uniform sampler2D dayTexture;
+    uniform sampler2D nightTexture;
+    uniform vec2 sunPosition;
+    uniform vec2 globeRotation;
+    varying vec3 vNormal;
+    varying vec2 vUv;
+
+    float toRad(in float a) {
+      return a * PI / 180.0;
+    }
+
+    vec3 Polar2Cartesian(in vec2 c) { // [lng, lat]
+      float theta = toRad(90.0 - c.x);
+      float phi = toRad(90.0 - c.y);
+      return vec3( // x,y,z
+        sin(phi) * cos(theta),
+        cos(phi),
+        sin(phi) * sin(theta)
+      );
+    }
+
+    void main() {
+      float invLon = toRad(globeRotation.x);
+      float invLat = -toRad(globeRotation.y);
+      mat3 rotX = mat3(
+        1, 0, 0,
+        0, cos(invLat), -sin(invLat),
+        0, sin(invLat), cos(invLat)
+      );
+      mat3 rotY = mat3(
+        cos(invLon), 0, sin(invLon),
+        0, 1, 0,
+        -sin(invLon), 0, cos(invLon)
+      );
+      vec3 rotatedSunDirection = rotX * rotY * Polar2Cartesian(sunPosition);
+      float intensity = dot(normalize(vNormal), normalize(rotatedSunDirection));
+      vec4 dayColor = texture2D(dayTexture, vUv);
+      vec4 nightColor = texture2D(nightTexture, vUv);
+      float blendFactor = smoothstep(-0.1, 0.1, intensity);
+      gl_FragColor = mix(nightColor, dayColor, blendFactor);
+    }
+  `,
+};
+
+/**
+ * @param date
+ * @returns the calculated longitudinal position of the sun for a given date
+ */
+const sunPosAt = (date: number) => {
+  const day = new Date(+date).setUTCHours(0, 0, 0, 0);
+  const t = solar.century(date);
+  const longitude = ((day - date) / 864e5) * 360 - 180;
+  return [longitude - solar.equationOfTime(t) / 4, solar.declination(t)];
+};
 
 export { Globe };
