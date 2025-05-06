ffce6bd9f08714591084cfc6732ea749f2fd9b7f
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-05-06 18:28:15 -0400

    document getMessagesWithin

--- server/db/queries.ts
+++ server/db/queries.ts
@@ -4,16 +4,22 @@ import { db } from './client';
 import { notes } from './schema';
 import { sql } from 'drizzle-orm';
 
+/**
+ * @param distance the distance **in meters** from the point.
+ * @param from the provided point. **Note:** *x* is longitude, *y* is latitude
+ * @param amount how many results to get.
+ * @returns the specified amount of results within the range of the provided point.
+ */
 export async function getMessagesWithin(
   distance: number,
-  from: typeof notes.$inferSelect.location, // in meters?
+  from: typeof notes.$inferSelect.location, // in meters
   amount = 20,
 ) {
   return await db
     .select({ message: notes.message, location: notes.location })
     .from(notes)
     .where(
-      sql`ST_DWithin(${notes.location}::geography, ST_SetSRID(ST_MakePoint(${from.x}, ${from.y}), 4326)::geography, ${distance})`,
+      sql`ST_DWithin(${notes.location}::geography, ST_Point( ${from.x}, ${from.y}, 4326)::geography, ${distance})`,
     )
     .limit(amount);
 }
