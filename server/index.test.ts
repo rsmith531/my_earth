4d66bca1acf0a75d4b3be09f78a633fd5817e09a
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-05-06 01:46:34 -0400

    tests for get method on save-note route

---
+++ server/index.test.ts
@@ -0,0 +1,82 @@
+// server\index.test.ts
+
+import { testClient } from 'hono/testing';
+import dbserver from './index';
+import { expect, test, describe, beforeAll, mock, afterAll } from 'bun:test';
+
+describe('The notes endpoint', () => {
+  const realConsole = console;
+  
+  beforeAll(() => {
+    global.console = {
+      ...console,
+      log: mock(),
+    };
+  });
+
+  afterAll(() => {
+    global.console = realConsole;
+  });
+  const client = testClient(dbserver);
+
+  const getParams = {
+    altitude: '4',
+    latitude: '41.15611',
+    longitude: '-81.41418',
+  };
+
+  describe('for GET requests', () => {
+    test('should return notes', async () => {
+      const res = await client['save-note'].$get({
+        query: getParams,
+      });
+
+      expect(res.status).toBe(200);
+
+      const body = await res.json();
+      console.log(body);
+      expect(body).toBeArray();
+      expect(body).not.toHaveProperty('error');
+    });
+    test('should respect the results param', async () => {
+      const res = await client['save-note'].$get({
+        query: { ...getParams, results: '1' },
+      });
+
+      expect(res.status).toBe(200);
+
+      const body = await res.json();
+      console.log(body);
+      expect(body).toBeArray();
+      expect(body).toHaveLength(1);
+      expect(body).not.toHaveProperty('error');
+    });
+
+    describe('should return errors', () => {
+      test('for missing params', async () => {
+        const res = await client['save-note'].$get({
+          // @ts-expect-error intentionally excluding altitude to test missing params
+          query: { ...getParams, altitude: undefined },
+        });
+
+        // @ts-expect-error middleware types can't be inferred
+        expect(res.status).toBe(400);
+        const body = await res.json();
+        expect(body).toHaveProperty('error');
+      });
+      test('for invalid params', async () => {
+        const res = await client['save-note'].$get({
+          query: {
+            ...getParams,
+            altitude: 'not a number',
+          },
+        });
+
+        // @ts-expect-error middleware types can't be inferred
+        expect(res.status).toBe(400);
+        const body = await res.json();
+        expect(body).toHaveProperty('error');
+      });
+    });
+  });
+});
