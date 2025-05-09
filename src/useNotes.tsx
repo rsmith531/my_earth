23007f93b98c815175b9cf3bc7e1802fdd25b848
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-05-09 00:09:03 -0400

    move note fetching into a react query

---
+++ src/useNotes.tsx
@@ -0,0 +1,42 @@
+// src\useNotes.tsx
+
+import { queryOptions, useQuery } from '@tanstack/react-query';
+import { honoClient } from '@lib/utils';
+import type { Home } from '@components/page/Home';
+
+export const notesQueryOptions = (keys: {
+  fov: number;
+  altitude: number;
+  latitude: number;
+  longitude: number;
+}) =>
+  queryOptions({
+    queryKey: ['notes', { ...keys }],
+    queryFn: async (): Promise<Parameters<typeof Home>[0]['notes']> => {
+      const response = await (
+        await honoClient()['save-note'].$get({
+          query: {
+            altitude: keys.altitude.toString(),
+            fieldOfView: keys.fov.toString(),
+            latitude: keys.latitude.toString(),
+            longitude: keys.longitude.toString(),
+          },
+        })
+      ).json();
+
+      if (Array.isArray(response)) {
+        return response.map((result) => {
+          return {
+            message: result.message,
+            latitude: result.location.y,
+            longitude: result.location.x,
+          };
+        });
+      }
+      throw new Error(response.error);
+    },
+    placeholderData: [],
+  });
+
+export const notesQuery = (keys: Parameters<typeof notesQueryOptions>[0]) =>
+  useQuery(notesQueryOptions(keys));
