1440ad52d4e113f2cc94696b0bf81286ba0b03a1
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-06-07 21:45:43 -0400

    extract markdown renderer from article component

--- client/components/page/Article.stories.tsx
+++ client/components/page/Article.stories.tsx
@@ -2,20 +2,26 @@
 
 import type { Meta, StoryObj } from '@storybook/react';
 import { Article } from './Article';
-// ?raw suffix reads the file at build time and embeds the string into the bundle
-import readmeContent from '../../../CONTRIBUTING.md?raw';
-
 
 const meta: Meta<typeof Article> = {
   component: Article,
   tags: ['!autodocs'],
-  parameters: {layout: 'fullscreen'}
+  parameters: { layout: 'fullscreen' },
 };
 
 type Story = StoryObj<typeof Article>;
 
 export const Default: Story = {
-  args: { content: readmeContent },
+  args: { children: <div>Hello, world</div> },
+};
+
+export const LotsOfChildren: Story = {
+  args: {
+    children: Array.from({ length: 150 }, (_, i) => (
+      // biome-ignore lint/suspicious/noArrayIndexKey: don't worry about it
+      <div key={i}>Child {i + 1}</div>
+    )),
+  },
 };
 
 export default meta;
--- client/components/page/Article.tsx
+++ client/components/page/Article.tsx
@@ -1,27 +1,16 @@
 // components\page\Article.tsx
 
-import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import rehypeRaw from 'rehype-raw';
 import { Globe } from '@components/section/Globe';
-import { Image } from '@components/ui/image';
-import { CodeBlock } from '@components/ui/codeblock';
-import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-
-// TODO: make sure URLs go to the hostname of the website
+import type { ReactNode } from 'react';
 
 /**
- * Renders a string of markdown onto a webpage. It replaces any `<img>` and
- * `<code>` with custom image and code block components to make sure they are
- * looking spiffy.
- *
- * It puts the page content into a styled container in front of a blurred globe
- * as a background.
+ * Renders page content in a styled container in front of a blurred globe as a
+ * background.
  */
-function Article({ content }: { content: string }) {
+function Article({ children }: { children: ReactNode }) {
   return (
     <div className="relative py-12 min-h-dvh flex items-center">
-      <div className="z-0 inset-0 absolute pointer-events-none h-full">
+      <div className="z-0 inset-0 fixed pointer-events-none h-dvh">
         <Globe
           interactive={false}
           reportViewpoint={() => {}}
@@ -41,45 +30,7 @@ function Article({ content }: { content: string }) {
         }}
         className=" relative m-auto bg-slate-900/95 max-w-4xl border-slate-200 border-2 z-10"
       >
-        <Markdown
-          remarkPlugins={[remarkGfm]}
-          rehypePlugins={[rehypeRaw]}
-          components={{
-            img(props) {
-              const { node, ...rest } = props;
-              return (
-                <Image width={rest.width} height={rest.height} {...rest} />
-              );
-            },
-            code(props) {
-              const { node, ...rest } = props;
-              console.log(rest.className, rest.children)
-              const match = rest.className?.match(/language-(\w+)/);
-              const language = match ? match[1] : null;
-              // TODO: write tests for this
-              if (!String(rest.children).trim().includes('\n')) {
-                return (
-                  <CodeBlock
-                    language={language ?? 'text'}
-                    style={atomDark}
-                    inline
-                    enableCopy={!!language}
-                  >{String(rest.children).trim()}</CodeBlock>
-                );
-              }
-              return (
-                <CodeBlock
-                  language={language ?? 'text'}
-                  style={atomDark}
-                  showLineNumbers
-                  enableCopy={!!language}
-                >{String(rest.children).trim()}</CodeBlock>
-              );
-            },
-          }}
-        >
-          {content}
-        </Markdown>
+        {children}
       </div>
     </div>
   );
